35b047d8e62c13eeafe275a4b0ab459e50d6be5d
Scion-Kin <dinturner17@gmail.com>
2023-03-18 06:40:06 -0700

    hry

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: 0 
+ */
+
+int main(void)
+{
+	int a = 97;
+	
+	while (a < 122)
+	{
+		putchar(a);
+		a++;
+		putchar('\n');
+	}
+	return (0);
+}
