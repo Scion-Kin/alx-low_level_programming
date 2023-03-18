16ce31a65b2e52299b06466e2bb85f56419ab3d8
Scion-Kin <dinturner17@gmail.com>
2023-03-18 13:14:50 -0700

    hey

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,13 @@ int main(void)
 	int a;
 	int b;
 
+	for (b = 48; b <= 57; b++)
+	{
+		putchar(b);
+	}
 	for (a = 97; a <= 102; a++)
 	{
 		putchar(a);
-		a++;
-	}
-	for (b = 49; a <= 57; b++)
-	{
-		putchar(b);
-		b++;
 	}
 	putchar('\n');
 	return (0);
