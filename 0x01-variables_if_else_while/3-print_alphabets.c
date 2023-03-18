286638e832d7638f21057c3ffdeee3013717e0cc
Scion-Kin <dinturner17@gmail.com>
2023-03-18 17:55:54 +0200

    Hey

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,19 @@
 
 int main(void)
 {
-        int a = 97;
+	int a = 97;
 	int b = 65;
 
 	while (a <= 122)
-        {
-                putchar(a);
-                a++;
-        }
+	{
+		putchar(a);
+		a++;
+	}
 	while (b <= 90)
 	{
 		putchar(b);
 		b++;
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
