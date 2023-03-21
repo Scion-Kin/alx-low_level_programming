1947ef473e6c176fd3c3652d91ecb06d158e5963
Scion-Kin <dinturner17@gmail.com>
2023-03-21 08:42:43 -0700

    hey

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,12 @@
 
 /**
  * _isalpha(int c) - checks if letter is upper or lowercase
- * c is a reference to the ascii code
- * Return: 1 (Yes), 0 (No)
+ * @c: reference to the ascii code
+ * Return: 1 (Yes) 0 (No)
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122) || (c >= 65 && c <= 90)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
