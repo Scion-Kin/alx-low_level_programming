6d739a902ce598584928cd96f7105dc98894cdcb
Scion-Kin <dinturner17@gmail.com>
2023-03-21 14:23:13 -0700

    hey

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@
 void times_table(void)
 {
 	int b, c, d, e, f;
+
 	for (d = 0; d <= 9; d++)
 	{
 		for (c = 0; c <= 9; c++)
@@ -15,18 +16,16 @@ void times_table(void)
 			if (e > 9)
 			{
 				b = e % 10;
-				f = (e - b)/10;
+				f = (e - b) / 10;
 				_putchar(f + 48);
 				_putchar(b + 48);
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			}
 			else
 			{
 				_putchar(e + 48);
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			}
 		}
