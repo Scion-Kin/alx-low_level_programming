7f59bc2a64d1f1e2b62ad261870146195a577e95
Scion-Kin <dinturner17@gmail.com>
2023-03-23 12:02:24 -0700

    hey

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main -prints 1 to 100 but prints fizz for multiples
+ * of 3 and buzz for multiples of 5, and fizzbuzz for
+ * multiples of both 3 and 5
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 100; a++)
+	{
+		if (a == 0)
+		{
+			continue;
+		}
+		if (a % 3 == 0 && a % 5 != 0)
+		{
+			printf("Fizz ");
+		}
+		else if (a % 5 == 0 && a % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+}
