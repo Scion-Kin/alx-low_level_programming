5b16abb60a4c9b53aa0d6141cbec8b3c9e1c2fe9
Scion-Kin <dinturner17@gmail.com>
2023-03-30 08:10:51 -0700

    hey

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,8 @@
 /**
  * string-toupper - changes all lowercase letters
  * of a string to uppercase.
- * Return: 0
+ * @a: input
+ * Return: a
  */
 char *string_toupper(char *a)
 {
@@ -11,9 +12,9 @@ char *string_toupper(char *a)
 	b = 0
 	while (a[b] != '\0')
 	{
-		if (a[b] == '90' && a[b] <= '122')
+		if (a[b] == 'a' && a[b] <= 'z')
 		{
-			a[b] = a[b] - '32';
+			a[b] = a[b] - 32;
 		}
 		a[b]++;
 	}
