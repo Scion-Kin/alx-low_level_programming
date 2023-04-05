cfc29610091ad80a49fa9e06c5d6c60a6fe9e83a
Scion-Kin <dinturner17@gmail.com>
2023-04-05 07:20:55 -0700

    hey

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,14 +7,12 @@
  *
  * Return: result of the power
  */
+
 int _pow_recursion(int x, int y)
 {
-	if (y >= 0)
-	{
-		return (x*(_pow_recursion(x*y)));
-	}
-	else
-	{
+	if (y < 0)
 		return (-1);
-	}
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
