37d54edccf5ee29c37bc27be76dccecc58c563b6
Scion_Kin <dinturner17@gmail.com>
2023-06-07 06:57:32 -0700

    New Change

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,18 +3,17 @@
 /**
  * factorial - returns the factorial of a number
  * @n: number to return the factorial from
- *
  * Return: factorial of n
  */
 
 int factorial(int n)
 {
 	int fac = 1;
+
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
+	if (n == 0)
 		return (1);
-	else
-		fac = fac * factorial(n - 1);
+	fac = n * factorial(n - 1);
 	return (fac);
 }
