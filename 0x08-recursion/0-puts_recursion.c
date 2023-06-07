6fd8e32ea56df51d85c94a7413d333f1f8798f87
Scion_Kin <dinturner17@gmail.com>
2023-06-07 05:08:20 -0700

    File one

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: input
+ * return: void
+ */
+ 
+ void _puts_recursion(char *s);
+ {
+	int k;
+	
+	k = s[k];
+	 
+ 	_putchar(k);
+ 	k++;
+ 	_puts_recursion(k);
+ 	if (k == NULL)
+ 	{
+ 		_putchar('\n');
+ 	}
+ }
