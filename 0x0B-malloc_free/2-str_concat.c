903e50d13fd8a4afd8182661ae089f28c2c4bbdb
Scion_Kin <dinturner17@gmail.com>
2023-06-12 08:23:23 -0700

    New change

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,19 +9,21 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *joined;
-	int i = 0, j = 0;
+	int i, j;
 	
 	if (s1 == NULL)
 		s1 = "";
-	while(s1[i] != '\0')
-		i++;
 	if (s2 == NULL)
 		s2 = "";
+	i = j = 0;
+	while(s1[i] != '\0')
+                i++;
 	while(s2[j] != '\0')
 		j++;
 	joined = malloc((i + j + 1) * sizeof(char));
 	if (joined == NULL)
 		return (NULL);
+	i = j = 0;
 	while (s1[i] != '\0')
 	{
 		joined[i] = s1[i];
