3a8f7f0a0376ea40d18377db018097bc443cb305
Scion_Kin <dinturner17@gmail.com>
2023-06-12 06:53:37 -0700

    New change

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include "main.h"
 
+int get_length(char *str);
+
 /**
  * str_concat - concatenates two strings
  * @s1: string one
  * @s2: string two
- * Return: Concatenated string
+ * Return: pointer to new string
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -13,15 +15,31 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, k, l;
 
 	if (s1 == NULL && s2 == NULL)
-		return(NULL);
-	if (s1 == NULL && s2 != NULL)
-		return (s2);
-	while (s1[i] != '\0' && s1[i] != ' ')
-		i++;
-	if (s2 == NULL && s1 != NULL)
-		return (s1);
-	while (s2[j] != '\0' && s2[j] != ' ')
-		j++;
+		return (NULL);
+	if (s1 == NULL)
+	{
+		j = get_length(s2);
+		joined = malloc((j + 1) * sizeof(char));
+		if (joined == NULL)
+			return (NULL);
+		for (l = 0; l <= j; l++)
+			joined[l] = s2[l];
+		joined[l] = '\0';
+		return (joined);
+	}
+	if (s2 == NULL)
+	{
+		i = get_length(s1);
+		joined = malloc((i + 1) * sizeof(char));
+		if (joined == NULL)
+			return (NULL);
+		for (k = 0; k <= i; k++)
+			joined[k] = s1[k];
+		joined[k] = '\0';
+		return (joined);
+	}
+	i = get_length(s1);
+	j = get_length(s2);
 	joined = malloc((i + j + 1) * sizeof(char));
 	if (joined == NULL)
 		return (NULL);
@@ -29,5 +47,19 @@ char *str_concat(char *s1, char *s2)
 		joined[k] = s1[k];
 	for (l = 0; l <= j; l++)
 		joined[k++] = s2[l];
-	return(joined);
+	joined[k] = '\0';
+	return (joined);
+}
+/**
+ * get_length - gets length
+ * @str: string
+ * Return: length
+ */
+int get_length(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+		length++;
+	return (length);
 }
