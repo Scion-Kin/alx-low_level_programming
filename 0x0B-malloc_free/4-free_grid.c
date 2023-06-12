88099ba490c5f9d033f233a4d6fcc1c0f2c53c96
Scion_Kin <dinturner17@gmail.com>
2023-06-12 12:20:29 -0700

    New Change

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - free memory occupied by grid
+ * @grid: argument
+ * @height: argument
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int s;
+
+	if (grid == NULL || height <= 0)
+		return;
+	for (s = 0; s < height; s++)
+		free(grid[s]);
+}
