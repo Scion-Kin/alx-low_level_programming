4afecf66724fa88af9c3cf83e353165540c1dd20
Scion_Kin <dinturner17@gmail.com>
2023-06-12 08:06:48 -0700

    New change

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,11 +23,15 @@ char *str_concat(char *s1, char *s2)
 	if (joined == NULL)
 		return (NULL);
 	while (s1[i] != '\0')
+	{
 		joined[i] = s1[i];
-		i++
+		i++;
+	}
 	while (s2[j] != '\0')
+	{
 		joined[i] = s2[j];
 		i++, j++;
+	}
 	joined[i] = '\0';
 	return(joined);
 }
