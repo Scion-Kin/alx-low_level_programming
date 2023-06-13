6e3cac84c9234094fa6742fad4b807cd451a152b
Scion_Kin <dinturner17@gmail.com>
2023-06-13 11:22:48 -0700

    New change

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector(but I like volt! haha)
+ * Return: 0 is success
+ */
+
+int main(argc, *argv[])
+{
+	int i = 0;
+
+	if (argc > 0)
+	{
+		printf("%s", argv[i]);
+	}
+}
