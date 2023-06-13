95a2398a83e98d446f474b64c2f0ec08fcc826e5
Scion_Kin <dinturner17@gmail.com>
2023-06-13 11:54:37 -0700

    New change

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the name of the program
@@ -7,9 +8,13 @@
  * Return: 0 is success
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
+	{
 		printf("%s\n", argv[0]);
+	}
+	else
+		exit (1);
 	return (0);
 }
