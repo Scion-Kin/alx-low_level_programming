fb7ce1c81c31c8b1999d1cbcbedf615d0ed43929
Scion Kin <dinturner17@gmail.com>
2023-06-15 07:57:34 -0700

    New file

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	array = malloc(dif * sizeof(int));
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i <= dif; i++)
+	for (i = 0; min <= max; i++)
 	{
 		array[i] = min;
 		min++;
