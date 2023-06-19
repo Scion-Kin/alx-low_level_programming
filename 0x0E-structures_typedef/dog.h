91c35e64819028c3a9304ddd2c767b8fed2915a9
Scion Kin <dinturner17@gmail.com>
2023-06-19 06:10:57 -0700

    New Change

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,22 @@
+#ifndef DOD_H
+#define DOG_H
+
+/**
+ * struct dog - A dog's details
+ * @name: the name
+ * @age: it's age
+ * @owner: it's owner
+ */
+
 struct dog{
 	char *name;
 	float age;
 	char *owner;
 }
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
