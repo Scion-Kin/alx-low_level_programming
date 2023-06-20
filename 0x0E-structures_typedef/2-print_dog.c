3a196864c7751677fca9cd36747440812b3c9520
Scion Kin <dinturner17@gmail.com>
2023-06-20 03:44:54 -0700

    New Change

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -5,21 +5,15 @@
 /**
  * print_dog - prints all the vars in struct dog
  * @d: struct variable pointer
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner name
  */
 
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL || d->age == NULL || d->owner == NULL)
-		printf("(nil)");
-	else
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %.6f\n", d->age);
-		printf("Owner: %s\n", d->owner);
-	}
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
