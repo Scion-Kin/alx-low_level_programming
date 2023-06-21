0fec5b59a44088b56c1f9aa146c0cedadab4beb6
Scion Kin <dinturner17@gmail.com>
2023-06-21 02:44:19 -0700

    New Change

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints the name of the function
+ * return: function pointer
+ * @name: name of the function
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	return (f(name));
+}
