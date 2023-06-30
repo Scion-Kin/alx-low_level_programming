1f81f0ef6d724f0f1ba98958ae71bddad531fa46
Scion Kin <dinturner17@gmail.com>
2023-06-30 09:31:46 -0700

    New Change

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns number of elements in a list
+ * @h: list
+ * Return: numbers of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+	const list_t *node_tra = h;
+
+	while (node_tra != NULL)
+	{
+		i++;
+		node_tra = node_tra->next;
+	}
+	return (i);
+}
