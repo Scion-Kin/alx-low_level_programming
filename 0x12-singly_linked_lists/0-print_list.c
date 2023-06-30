8693fa6b2a64b001f02aaf6239375a280c373889
Scion Kin <dinturner17@gmail.com>
2023-06-30 09:16:18 -0700

    New Change

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,13 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	int i = 0;
 	const list_t *node_tra = h;
+
+	int i = 0;
 
 	while (node_tra != NULL)
 	{
 		if (node_tra->str == NULL)
-			printf("[0] (nil)");
+		{
+			printf("[0] (nil)\n");
+			i++;
+		}
 		else
 		{
 			printf("[%d] %s\n", node_tra->len, node_tra->str);
