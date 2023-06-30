d64de36607ac93653b377de2d71a66c639fbb9f8
Scion Kin <dinturner17@gmail.com>
2023-06-30 14:06:02 -0700

    New File

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: current node
+ * Return: last node
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
