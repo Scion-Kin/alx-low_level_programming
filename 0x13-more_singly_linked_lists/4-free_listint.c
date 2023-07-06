220bee2ed2440687471e8604f98283184d6d3f7d
Scion Kin <dinturner17@gmail.com>
2023-07-06 06:19:57 -0700

    New Change

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a list
+ * @head: list
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
