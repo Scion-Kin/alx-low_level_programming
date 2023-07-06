fe3aefeafaf4e84688a159c5bded0014d2426607
Scion Kin <dinturner17@gmail.com>
2023-07-06 03:57:16 -0700

    New Change

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,23 +11,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	const list_t *node_tra = h;
+	const listint_t *c = h;
+	size_t i = 0;
 
-	int i = 0;
-
-	while (node_tra != NULL)
+	while (c != NULL)
 	{
-		if (node_tra->str == NULL)
-		{
-			printf("[0] (nil)\n");
-			i++;
-		}
-		else
-		{
-			printf("[%d] %s\n", node_tra->len, node_tra->str);
-			i++;
-		}
-		node_tra = node_tra->next;
+		printf("%d\n", c->n);
+		c = c->next;
+		i++;
 	}
 	return (i);
 }
