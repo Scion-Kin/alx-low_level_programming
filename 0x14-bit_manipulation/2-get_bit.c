decd5eaa1275b912612a0b46481729e5736cd7e7
Scion Kin <dinturner17@gmail.com>
2023-07-10 08:28:33 -0700

    New Change

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * get_bit - returns the value of a bit at a given index
@@ -9,11 +9,14 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int mask;
+	int bit_value;
+
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	unsigned long int mask = 1UL << index;
-	int bit_value = (n & mask) != 0;
+	mask = 1UL << index;
+	bit_value = (n & mask) != 0;
 
-	return bit_value;
+	return (bit_value);
 }
