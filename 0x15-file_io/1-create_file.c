aa943d2db541686919bd21042c1a3376f1bd9dca
Scion Kin <dinturner17@gmail.com>
2023-07-13 06:51:22 -0700

    New Change

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file, sets permissions and writes in file
+ * @filename: file name to create
+ * @text_content: text to write
+ * Return: 1 on success, -1 on failure
+ */
+ 
+int create_file(const char *filename, char *text_content)
+{
+	int fd, written, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+			i++;
+
+		written = write(fd, text_content, i);
+		if (written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
