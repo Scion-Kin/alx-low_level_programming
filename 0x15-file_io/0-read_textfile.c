6ef640c846f028af517f7e35453ca3d316ea9c56
Scion Kin <dinturner17@gmail.com>
2023-07-13 05:40:30 -0700

    New Change

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,36 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters to read and print
- *
- * Return: actual number of letters read and printed, or 0 on failure
+ * read_textfile - reads text file and outputs to stdout
+ * @filename: file name
+ * @letters: number of letters to read
+ * Return: number of characters written
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t total_bytes_read = 0, bytes_read, bytes_written;
-	char buffer[1024];
-
-	if (filename == NULL)
-		return (0);
+	char buffer[letters + 1];
+	ssize_t read_c, written;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	while (letters > 0)
+	read_c = read(fd, buffer, letters);
+	if (read_c == -1)
 	{
-		bytes_read = read(fd, buffer, sizeof(buffer));
-		if (bytes_read == -1)
-			break;
+		close(fd);
+		return (0);
+	}
 
-		if (bytes_read == 0)  // End of file reached
-			break;
-
-		if (letters < bytes_read)
-			bytes_read = letters;
-
-		bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-		if (bytes_written == -1)
-			break;
-
-		total_bytes_read += bytes_read;
-		letters -= bytes_read;
+	written = write(STDOUT_FILENO, buffer, read_c);
+	if (written == -1 || written != read_c)
+	{
+		close(fd);
+		return (0);
 	}
 
 	close(fd);
-	return (total_bytes_read);
+	return (written);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define O_RDONLY  0
+#define STDOUT_FILENO 1
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
