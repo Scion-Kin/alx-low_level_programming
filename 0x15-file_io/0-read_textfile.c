84e76b48abc47bb3960a685b9b5dbf22e3248b76
Scion Kin <dinturner17@gmail.com>
2023-07-13 05:52:44 -0700

    New Change

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,16 +10,24 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char buffer[letters + 1];
+	char *buffer;
 	ssize_t read_c, written;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
 	read_c = read(fd, buffer, letters);
 	if (read_c == -1)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
@@ -27,10 +35,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	written = write(STDOUT_FILENO, buffer, read_c);
 	if (written == -1 || written != read_c)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 
 	close(fd);
+	free(buffer);
 	return (written);
 }
