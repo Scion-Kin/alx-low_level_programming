8a862421b3fc462c3e19e6dad8493ae65c67511d
Scion-Kin <dinturner17@gmail.com>
2023-11-09 21:51:27 +0200

    Double

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-
 	if (idx == 0)
 	{
 		new_node->prev = NULL;
@@ -30,28 +29,22 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-
 	current = *h;
 	for (i = 0; i < idx - 1 && current != NULL; i++)
 	{
 		current = current->next;
 	}
-
 	if (current == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->prev = current;
 	new_node->next = current->next;
-
 	if (current->next != NULL)
 	{
 		current->next->prev = new_node;
 	}
-
 	current->next = new_node;
-
 	return (new_node);
 }
