2e18e9f2b0d92ab3347ed6cfd58f2db09dcbb534
Scion-Kin <dinturner17@gmail.com>
2023-11-09 18:46:20 +0200

    Double

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -10,16 +10,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current = head;
 
-	while (current->prev != NULL)
-	{
-		head = current->prev;
-		free(current);
-	}
-	while (current->next != NULL)
+	while (current->next)
 	{
 		head = current->next;
 		free(current);
+		current = head;
 	}
-	free(current);
-	return (NULL);
 }
