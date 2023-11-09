32cf71fbe677a02e8ed796ac8e2e5bc0b106c9e1
Scion-Kin <dinturner17@gmail.com>
2023-11-09 15:24:44 +0200

    Double

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,19 +8,19 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-        size_t node_count = 0;
+	size_t node_count = 0;
 
-        while (h && h->prev)
-        {
-                h = h->prev;
-        }
+	while (h && h->prev)
+	{
+		h = h->prev;
+	}
 
-        while (h)
-        {
-                printf("%d\n", h->n);
-                h = h->next;
-                node_count++;
-        }
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
 
-        return (node_count);
+	return (node_count);
 }
