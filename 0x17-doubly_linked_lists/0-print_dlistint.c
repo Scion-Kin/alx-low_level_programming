ee0d47eebf14c8a20bb762182c409341f4ea32da
Scion-Kin <dinturner17@gmail.com>
2023-11-09 15:07:10 +0200

    Double

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the nodes of a list
+ * @h: the linked list
+ * Return: the number of nodes in the list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+        size_t node_count = 0;
+
+        while (h && h->prev)
+        {
+                h = h->prev;
+        }
+
+        while (h)
+        {
+                print("%d\n", h->n);
+                h = h->next;
+                node_count++;
+        }
+
+        return (node_count);
+}
