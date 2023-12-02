fc179b62c2ef790f95447d7f1ab42d174d61101b
Scion-Kin <dinturner17@gmail.com>
2023-12-02 22:26:48 +0200

    Objects

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,11 +9,26 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new;
+	unsigned long int i = 0;
 
-	new = malloc(sizeof(hash_table_t) * size);
-
+	new = malloc(sizeof(hash_table_t));
 	if (new == NULL)
 		return NULL;
+
+	new->array = malloc(sizeof(hash_table_t) * size);
+	if (new->array == NULL)
+	{
+		free(new);
+		return NULL;
+	}
+
+	for (; i < size;)
+	{
+		new->array[i] = NULL;
+		i++;
+	}
+
+	new->size = size;
 
 	return (new);
 }
