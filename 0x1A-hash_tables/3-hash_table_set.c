651b1ceb234b740a3e1afbc8654d584d2a995f98
Scion-Kin <dinturner17@gmail.com>
2023-12-03 21:49:08 +0200

    Hash_Tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,10 +1,42 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - inserts a node into a hash table
+ * bring_node - makes a new for a hash table
+ * @key: the key
+ * @value: the value
+ * Return: the new new
+ */
+
+hash_node_t *bring_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (NULL);
+	}
+	return (new);
+}
+
+/**
+ * hash_table_set - inserts a new into a hash table
  * @ht: the table to insert into
- * @key: the key for the node
- * @value: the value of the node
+ * @key: the key for the new
+ * @value: the value of the new
  * Return: 1 on success and 0 on failure
 */
 
@@ -13,26 +45,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *current;
 	unsigned long int index;
 
+	node = bring_node(key, value);
+
 	if (key == NULL || value == NULL || ht == NULL)
 		return (0);
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (0);
-	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (0);
-	}
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (0);
-	}
+
 	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
+
 	while (current != NULL)
 	{
 		if (strcmp(current->key, key) == 0)
@@ -50,7 +70,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
+
 	node->next = ht->array[index];
 	ht->array[index] = node;
+
 	return (1);
 }
